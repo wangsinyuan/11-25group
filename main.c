084abc40def68520441354251ed40b7cf0cb4a08
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-11-25 22:58:45 +0800

    finish float_add

--- main.c
+++ main.c
@@ -5,9 +5,17 @@ int add_int(int a,int b)
     return a+b;
 }
 
+float add_float(float a,float b)
+{
+    return a+b;
+}
+
 int main(){
     int a=5;
     int b=13; 
+    float fa=5.5;
+    float fb=13.2;
     printf("%d + %d = %d\n",a,b,add_int(a,b));
+    printf("%f +%f = %f\n",fa,fb,add_float(fa,fb));
     return 0;
 }
