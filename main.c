04132712c758e3f77b93f5a37369c2abfca0edf1
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-11-25 22:50:46 +0800

    finish int_add

--- main.c
+++ main.c
@@ -1,6 +1,13 @@
 #include<stdio.h>
 
+int add_int(int a,int b)
+{
+    return a+b;
+}
+
 int main(){
-    printf("Hello World\n");
+    int a=5;
+    int b=13; 
+    printf("%d + %d = %d\n",a,b,add_int(a,b));
     return 0;
 }
