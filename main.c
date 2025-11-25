42fc63700f13ac74ccc11f42e546221b37abdc24
wangsinyuan <41475904h@gapps.ntnu.edu.tw>
2025-11-25 22:55:27 +0800

    finish int_sub

--- main.c
+++ main.c
@@ -5,9 +5,15 @@ int add_int(int a,int b)
     return a+b;
 }
 
+int sub_int(int a,int b)
+{
+    return a-b;
+}
+
 int main(){
     int a=5;
     int b=13; 
     printf("%d + %d = %d\n",a,b,add_int(a,b));
+    printf("%d - %d = %d\n",a,b,sub_int(a,b));
     return 0;
 }
